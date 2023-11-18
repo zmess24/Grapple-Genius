893089baeda4276d1980914e14280dd73851b926
Zac Messinger <zdmessinger@gmail.com>
2023-11-18 10:59:38 -0800

    updated

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -6,80 +6,70 @@ import tw from "twrnc";
 const people = [
 	{
 		type: "Open Matt",
-		email: "leslie.alexander@example.com",
-		role: "Co-Founder / CEO",
+		focus: "",
 		imageUrl:
 			"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-18",
 	},
 	{
 		type: "Class",
-		email: "michael.foster@example.com",
-		role: "Co-Founder / CTO",
+		focus: "Bottom Half Guard",
 		imageUrl:
 			"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-17",
 	},
 	{
 		type: "Class",
-		email: "dries.vincent@example.com",
-		role: "Business Relations",
+		focus: "dries.vincent@example.com",
 		imageUrl:
 			"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-16",
 	},
 	{
 		type: "Class",
-		email: "lindsay.walton@example.com",
-		role: "Front-end Developer",
+		focus: "lindsay.walton@example.com",
 		imageUrl:
 			"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-15",
 	},
 	{
 		type: "Competition",
-		email: "courtney.henry@example.com",
-		role: "Designer",
+		focus: "courtney.henry@example.com",
 		imageUrl:
 			"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-14",
 	},
 	{
 		type: "Private",
-		email: "tom.cook@example.com",
-		role: "Director of Product",
+		focus: "tom.cook@example.com",
 		imageUrl:
 			"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-13",
 	},
 	{
 		type: "Class",
-		email: "lindsay.walton@example.com",
-		role: "Front-end Developer",
+		focus: "lindsay.walton@example.com",
 		imageUrl:
 			"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-12",
 	},
 	{
 		type: "Class",
-		email: "lindsay.walton@example.com",
-		role: "Front-end Developer",
+		focus: "lindsay.walton@example.com",
 		imageUrl:
 			"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-11",
 	},
 	{
 		type: "Class",
-		email: "lindsay.walton@example.com",
-		role: "Front-end Developer",
+		focus: "lindsay.walton@example.com",
 		imageUrl:
 			"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-10",
 	},
 	{
 		type: "Class",
-		email: "lindsay.walton@example.com",
-		role: "Front-end Developer",
+		focus: "lindsay.walton@example.com",
 		imageUrl:
 			"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 		date: "2023-11-09",
